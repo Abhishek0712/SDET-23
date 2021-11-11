1abc66cb7b9027b20e005f507c3e8d902a9476ad
ADMIN <ADMIN@DESKTOP-TS1AGTI>
2021-11-11 12:59:09 +0530

    my first commit

---
+++ Gitpractise/src/test/java/gitpractise/Demo1.java
@@ -0,0 +1,11 @@
+package gitpractise;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	@Test
+	public void demo() {
+		System.out.println("this is my first commit");
+	}
+
+}
---
+++ Gitpractise/src/test/java/gitpractise/Demo2.java
@@ -0,0 +1,11 @@
+package gitpractise;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	public void demo2() {
+		System.out.println("not happening");
+	}
+
+}
